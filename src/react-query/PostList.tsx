bf19c9268ae7e8da8bb963616fa1be375e661d51
Glenn Seymon <glenn@superdudes.com.au>
2023-12-04 21:55:19 +1100

    Parameterised queries

--- src/react-query/PostList.tsx
+++ src/react-query/PostList.tsx
@@ -1,7 +1,9 @@
+import { useState } from "react";
 import usePosts from "./hooks/usePosts";
 
 const PostList = () => {
-  const { data: posts, error, isLoading } = usePosts();
+  const [userId, setUserId] = useState<number>();
+  const { data: posts, error, isLoading } = usePosts(userId);
 
   if (isLoading) return <h1>Loading, loading, loading....</h1>;
 
@@ -9,6 +11,17 @@ const PostList = () => {
 
   return (
     <>
+      <select
+        onChange={(event) => setUserId(parseInt(event.target.value))}
+        value={userId}
+        className="form-select mb-3"
+      >
+        <option value=""></option>
+        <option value="1">User 1</option>
+        <option value="2">User 2</option>
+        <option value="3">User 3</option>
+      </select>
+
       <h1>Posts</h1>
       <ul className="list-group">
         {posts.map((post) => (
