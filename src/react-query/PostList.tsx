6ae0fa033358c80794ff14b847a9a170e7e1241d
Glenn Seymon <glenn@superdudes.com.au>
2023-12-04 23:38:46 +1100

    Paginated queries

--- src/react-query/PostList.tsx
+++ src/react-query/PostList.tsx
@@ -2,8 +2,10 @@ import { useState } from "react";
 import usePosts from "./hooks/usePosts";
 
 const PostList = () => {
-  const [userId, setUserId] = useState<number>();
-  const { data: posts, error, isLoading } = usePosts(userId);
+  const pageSize = 10;
+  const [page, setPage] = useState(1);
+
+  const { data: posts, error, isLoading } = usePosts({ page, pageSize });
 
   if (isLoading) return <h1>Loading, loading, loading....</h1>;
 
@@ -11,17 +13,6 @@ const PostList = () => {
 
   return (
     <>
-      <select
-        onChange={(event) => setUserId(parseInt(event.target.value))}
-        value={userId}
-        className="form-select mb-3"
-      >
-        <option value=""></option>
-        <option value="1">User 1</option>
-        <option value="2">User 2</option>
-        <option value="3">User 3</option>
-      </select>
-
       <h1>Posts</h1>
       <ul className="list-group">
         {posts.map((post) => (
@@ -30,6 +21,20 @@ const PostList = () => {
           </li>
         ))}
       </ul>
+
+      <button
+        disabled={page === 1}
+        className="btn btn-primary my-3"
+        onClick={() => setPage(page - 1)}
+      >
+        Previous
+      </button>
+      <button
+        className="btn btn-primary my-3 ms-1"
+        onClick={() => setPage(page + 1)}
+      >
+        Next
+      </button>
     </>
   );
 };
